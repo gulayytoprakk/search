692446796f650fc1be7b1903a0ec5860a7a93d7b
Gülay TOPRAK <140807385+gulayytoprakk@users.noreply.github.com>
2023-07-28 17:07:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümle Yazınız:");
+            string word=Console.ReadLine();
+
+            Console.WriteLine("Aradığınız Kelime:");
+            string search=Console.ReadLine();
+            if (word.Contains(search))
+            {
+                if (word.StartsWith(search)) Console.WriteLine("Kelime Baştadır");
+                else if (word.EndsWith(search)) Console.WriteLine("Kelimeniz Sondadır");
+                else Console.WriteLine("Kelime Ortadadır.");
+            }
+            else Console.WriteLine("Kelime Bulunamadı");
+            Console.ReadKey();
+
+        }
+    }
+}
